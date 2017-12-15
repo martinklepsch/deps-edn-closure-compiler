72364a4c538c6231e448d8671b662e689edd8d9e
Martin Klepsch <martinklepsch@googlemail.com>
2017-12-15 18:03:40 +0100

    minimize repro

--- build.clj
+++ build.clj
@@ -1,10 +1,1 @@
 (require 'cljs.build.api)
-
-(cljs.build.api/build 
-  ["src"]
-  {:compiler-stats true
-   ;; :verbose     true
-   :output-to     "target/android/app.js"
-   :main          'app.core
-   :output-dir    "target/android"
-   :optimizations :none})
--- src/app/core.cljs
+++
@@ -1,3 +0,0 @@
-(ns app.core)
-
-(js/console.log "print")
